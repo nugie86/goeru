a842c052b7d7fd681d860601e89ef76e62b865e6
nugie86 <149038743+nugie86@users.noreply.github.com>
2023-12-21 15:00:11 +0700

    Delete backend/v1 directory

--- backend/v1/middleware/upload.js
+++
@@ -1,6 +0,0 @@
-// app/middleware/uploadMiddleware.js
-const multer = require('multer');
-
-const uploadMiddleware = multer({ storage: multer.memoryStorage() });
-
-module.exports = uploadMiddleware;
